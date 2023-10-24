ff88e264e3141b4c3768ceb51af9922bfa1c7935
ghy <ghyghoo8@qq.com>
2023-10-24 14:16:46 +0800

    feat: cc

--- rga/Conversational Retrieval Chain.ts
+++ rga/Conversational Retrieval Chain.ts
@@ -9,6 +9,7 @@ import { HNSWLib } from "langchain/vectorstores/hnswlib";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { StringOutputParser } from "langchain/schema/output_parser";
 
+// 实例化 llm
 const model = new ChatOpenAI({});
 
 const condenseQuestionTemplate = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
@@ -17,10 +18,12 @@ Chat History:
 {chat_history}
 Follow Up Input: {question}
 Standalone question:`;
+// 创建 prompt模板
 const CONDENSE_QUESTION_PROMPT = PromptTemplate.fromTemplate(
   condenseQuestionTemplate
 );
 
+// 创建正向 prompt
 const answerTemplate = `Answer the question based only on the following context:
 {context}
 
@@ -28,11 +31,13 @@ Question: {question}
 `;
 const ANSWER_PROMPT = PromptTemplate.fromTemplate(answerTemplate);
 
+// combo 多个序列化文档
 const combineDocumentsFn = (docs: Document[], separator = "\n\n") => {
   const serializedDocs = docs.map((doc) => doc.pageContent);
   return serializedDocs.join(separator);
 };
 
+// 历史聊天格式化
 const formatChatHistory = (chatHistory: [string, string][]) => {
   const formattedDialogueTurns = chatHistory.map(
     (dialogueTurn) => `Human: ${dialogueTurn[0]}\nAssistant: ${dialogueTurn[1]}`
@@ -40,6 +45,7 @@ const formatChatHistory = (chatHistory: [string, string][]) => {
   return formattedDialogueTurns.join("\n");
 };
 
+// 词向量
 const vectorStore = await HNSWLib.fromTexts(
   [
     "mitochondria is the powerhouse of the cell",
@@ -48,6 +54,7 @@ const vectorStore = await HNSWLib.fromTexts(
   [{ id: 1 }, { id: 2 }],
   new OpenAIEmbeddings()
 );
+// 获取查找器
 const retriever = vectorStore.asRetriever();
 
 type ConversationalRetrievalQAChainInput = {
@@ -55,17 +62,24 @@ type ConversationalRetrievalQAChainInput = {
   chat_history: [string, string][];
 };
 
+// 问题运行队列
 const standaloneQuestionChain = RunnableSequence.from([
+  // 模板 fields： {}
   {
     question: (input: ConversationalRetrievalQAChainInput) => input.question,
     chat_history: (input: ConversationalRetrievalQAChainInput) =>
       formatChatHistory(input.chat_history),
   },
+  // 模板实例
   CONDENSE_QUESTION_PROMPT,
+  // llm
   model,
+  // 输出格式
   new StringOutputParser(),
 ]);
 
+// 答案运行队列
+// Represents a chunk of a message
 const answerChain = RunnableSequence.from([
   {
     context: retriever.pipe(combineDocumentsFn),
